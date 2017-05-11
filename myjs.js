0feafc56f51f97f02b26a1c3b0f4419491cc7f7f
James Doucette <james.doucette@greenwichschools.org>
2017-05-10 22:06:49 -0400

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -5,17 +5,18 @@ var selectedFile;
 
 $( document ).ready(function() {
 	$("#welcome").hide();
+	$("#uploadButton").hide();
 	$(".upload-group").hide();
-	document.getElementById("upload").addEventListener('change', handleFileSelect, false);
 });
 
 function signIn() {
 	firebase.auth().signInWithPopup(provider).then(function(result) {
 	  // This gives you a Google Access Token. You can use it to access the Google API.
-	  var token = result.credential.accessToken;
+	  var token = result.user.uid;
 	  // The signed-in user info.
 	  user = result.user;
 	  showWelcomeContainer();
+	  sessionStorage.token = token;
 	  // ...
 	}).catch(function(error) {
 	  // Handle Errors here.
@@ -33,43 +34,58 @@ function signIn() {
 function showWelcomeContainer() {
 	$("#login").hide();
 	$("#welcome").show();
-	$("#welcomeText").html("Welcome Back " + user.displayName + "!");
-};
-
-
-function handleFileSelect(event) {
 	$(".upload-group").show();
-	selectedFile = event.target.files[0];
+	$("#welcomeText").html("Hello, " + user.displayName);
 };
 
-function confirmUpload() {
-	var metadata = {
-		contentType: 'image',
-		customMetadata: {
-			'dogType': 'Lab',
-			'uploadedBy': user.uid,
-			'title': $("#imgTitle").val(),
-			'caption': $("#imgDesc").val()
-		},
-	};
-	var uploadTask = firebase.storage().ref().child('dogImages/' + selectedFile.name).put(selectedFile, metadata);
+$(".dropdown").on("hide.bs.dropdown", function(event){
+    var text = $(event.relatedTarget).text(); // Get the text of the element
+    $("#dogDrop").html(text+'<span class="caret"></span>');
+    firebase.database().ref('Users/' + user.uid).set({
+    	name: user.displayName,
+    	email: user.email,
+    	favDog: text
+  	});
+
+});
+
+$("#file").on("change", function(event) {
+	selectedFile = event.target.files[0];
+	$("#uploadButton").show();
+});
+
+function uploadFile() {
+	// Create a root reference
+	var filename = selectedFile.name;
+	var storageRef = firebase.storage().ref('/dogImages/' + filename);
+	var uploadTask = storageRef.put(selectedFile);
+
 	// Register three observers:
 	// 1. 'state_changed' observer, called any time the state changes
 	// 2. Error observer, called on failure
 	// 3. Completion observer, called on successful completion
 	uploadTask.on('state_changed', function(snapshot){
-  		// Observe state change events such as progress, pause, and resume
-  		// See below for more detail
+	  // Observe state change events such as progress, pause, and resume
+	  // See below for more detail
 	}, function(error) {
-  		// Handle unsuccessful uploads
+	  // Handle unsuccessful uploads
 	}, function() {
-  		// Handle successful uploads on complete
-  		// For instance, get the download URL: https://firebasestorage.googleapis.com/...
-  		$(".upload-group")[0].before("Success!");
-  		$(".upload-group").hide();
-
+	  // Handle successful uploads on complete
+	  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+	  var postKey = firebase.database().ref('Posts/').push().key;
+	  var downloadURL = uploadTask.snapshot.downloadURL;
+	  var updates = {};
+	  var postData = {
+	  	url: downloadURL,
+	  	caption: $("#imageCaption").val(),
+	  	user: user.uid
+	  };
+	  updates['/Posts/'+postKey] = postData;
+	  firebase.database().ref().update(updates);
 	});
 
 }
+
+
 
 
