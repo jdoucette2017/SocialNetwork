eea920ae7a6592825434938e0d6c53744854db90
James Doucette <james.doucette@greenwichschools.org>
2017-05-10 22:00:01 -0400

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -33,19 +33,9 @@ function signIn() {
 function showWelcomeContainer() {
 	$("#login").hide();
 	$("#welcome").show();
-	$("#welcomeText").html("Hello, " + user.displayName);
+	$("#welcomeText").html("Welcome Back " + user.displayName + "!");
 };
 
-$(".dropdown").on("hide.bs.dropdown", function(event){
-    var text = $(event.relatedTarget).text(); // Get the text of the element
-    $("#dogDrop").html(text+'<span class="caret"></span>');
-    firebase.database().ref('Users/' + user.uid).set({
-    	name: user.displayName,
-    	email: user.email,
-    	favDog: text
-  	});
-
-});
 
 function handleFileSelect(event) {
 	$(".upload-group").show();
